0b7b9e4d7b75232379d675bbd9278cd5df07e904
EvgeniyBespalov <bespalov1299@mail.ru>
2018-02-21 21:08:36 +0300

    default path blockchain

--- blockchain_test.rb
+++ blockchain_test.rb
@@ -7,8 +7,10 @@ require_relative 'blockexplorer'
 class BlockchainOperation
   attr_reader:block
   
-  def initialize
-    @block = Blockchain::BlockExplorer.new
+  #http://localhost:3000/ for work with localhost 
+  #
+  def initialize blockchain_path = "http://localhost:3000/"
+    @block = Blockchain::BlockExplorer.new blockchain_path
   end
   
   #returned hash include transactions with bad address
@@ -56,7 +58,7 @@ class BlockchainOperation
 
 end
 
-oper = BlockchainOperation.new
+oper = BlockchainOperation.new "https://blockchain.info/"
 puts oper.valid_transaction "4ff162eceb4392a38f269aab1ee1df567ae06875042b3ab6b53bd334c2e9e888"
 
 
