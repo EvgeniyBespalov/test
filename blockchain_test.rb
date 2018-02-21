8bcddf09d8fb1f032107e03e2f40780210f49e0d
EvgeniyBespalov <bespalov1299@mail.ru>
2018-02-21 22:18:50 +0300

    include DataProvider

--- blockchain_test.rb
+++ blockchain_test.rb
@@ -4,13 +4,30 @@
 
 require_relative 'blockexplorer'
 
+
+class DataProvider
+  attr_reader:addresses_bad
+  
+  def initialize
+    @addresses_bad = []
+  end
+  
+  def address_bad_insert address
+    @addresses_bad << address
+  end
+  
+  
+end
+
+
 class BlockchainOperation
   attr_reader:block
-  
+  attr_reader:data_provider
   #http://localhost:3000/ for work with localhost 
   #
   def initialize blockchain_path = "http://localhost:3000/"
     @block = Blockchain::BlockExplorer.new blockchain_path
+    @data_provider = DataProvider.new
   end
   
   #returned hash include transactions with bad address
@@ -23,28 +40,22 @@ class BlockchainOperation
     trans.each do |t|
       trans = Blockchain::BlockExplorer.new.get_tx_by_hash tx_hash   
       
-      result = result.merge trans_addr_verify(trans.hash, trans.inputs)
-      result = result.merge trans_addr_verify(trans.hash, trans.outputs)
+      result = result.merge trans_addr_verify(@data_provider, trans.hash, trans.inputs)
+      result = result.merge trans_addr_verify(@data_provider, trans.hash, trans.outputs)
       
     end
       
     result
   end
-
+  
   private
   
-  def addresses_bad_get
-    #fictive address array
-    addresses = ['14UEUL1DhGGKDB15g8QdSwyLFtJBniaBbq']
-  end
-  
-  def trans_addr_verify trans, trans_addrs
+  def trans_addr_verify data_provider, trans, trans_addrs
     
-    result = Hash.new
-    addresses_bad = addresses_bad_get 
+    result = Hash.new 
     
     trans_addrs.each do |a|
-      if addresses_bad.find {|b| b == a.address}
+      if data_provider.addresses_bad.find {|b| b == a.address}
         if result[trans] == nil    
           result[trans] = [a.address]
         else
@@ -58,10 +69,11 @@ class BlockchainOperation
 
 end
 
+
+
 oper = BlockchainOperation.new "https://blockchain.info/"
+oper.data_provider.address_bad_insert "14UEUL1DhGGKDB15g8QdSwyLFtJBniaBbq"
 puts oper.valid_transaction "4ff162eceb4392a38f269aab1ee1df567ae06875042b3ab6b53bd334c2e9e888"
-
-
 
 
 
